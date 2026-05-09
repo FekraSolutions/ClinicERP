639cf0c40c7dda2a026ffa2a5b5adebcba730546
Mazen <fekrabusinesssolutions@gmail.com>
2026-05-09 20:56:35 +0300

    gitignore fixed

--- ClinicERP/src/main.jsx
+++ ClinicERP/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios' // This is the missing "introduction"
 import AuthProvider from './auth/AuthContext'
 import App from './App'
 
-
+// This tells your frontend where to send the data
+axios.defaults.baseURL = "https://clinic-erp-beta.vercel.app";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
